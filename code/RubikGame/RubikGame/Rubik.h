9800798fd67c34e45ff6c8dfa3335301237274b7
Alexander Francisco Villacres Guerrero <afvillacres@espe.edu.ec>
2024-08-17 12:20:52 -0500

    Actualizacion 1
    Actualizacion de menu y movimientos

--- code/RubikGame/RubikGame/Rubik.h
+++ code/RubikGame/RubikGame/Rubik.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 
 using namespace std;
@@ -39,12 +40,82 @@ public:
 		linea = intercambiar(linea, left[0]);
 	}
 
+	void rotarMed() {
+		int* linea = new int[3];
+		linea = intercambiar(left[1], front[1]);
+		linea = intercambiar(linea, right[1]);
+		linea = intercambiar(linea, back[1]);
+		linea = intercambiar(linea, left[1]);
+	}
+
 	void rotarBottom() {
 		int* linea = new int[3];
 		linea = intercambiar(left[2], front[2]);
 		linea = intercambiar(linea, right[2]);
 		linea = intercambiar(linea, back[2]);
 		linea = intercambiar(linea, left[2]);
+	}
+
+	void rotarIzquierda() {
+		rotarCara(left);
+		int temp[3];
+		for (int i = 0; i < 3; i++) {
+			temp[i] = top[i][0];
+			top[i][0] = front[i][0];
+			front[i][0] = bottom[i][0];
+			bottom[i][0] = back[2 - i][2];
+			back[2 - i][2] = temp[i];
+		}
+	}
+
+	void rotarDerecha() {
+		rotarCara(right);
+		int temp[3];
+		for (int i = 0; i < 3; i++) {
+			temp[i] = top[i][2];
+			top[i][2] = back[i][0];
+			back[i][0] = bottom[i][2];
+			bottom[i][2] = front[2 - i][2];
+			front[2 - i][2] = temp[i];
+		}
+	}
+
+	void rotarArriba() {
+		rotarCara(top);
+		int temp[3];
+		for (int i = 0; i < 3; i++) {
+			temp[i] = front[0][i];
+			front[0][i] = left[0][i];
+			left[0][i] = back[0][i];
+			back[0][i] = right[0][i];
+			right[0][i] = temp[i];
+		}
+	}
+
+	void rotarAbajo() {
+		rotarCara(bottom);
+		int temp[3];
+		for (int i = 0; i < 3; i++) {
+			temp[i] = front[2][i];
+			front[2][i] = right[2][i];
+			right[2][i] = back[2][i];
+			back[2][i] = left[2][i];
+			left[2][i] = temp[i];
+		}
+	}
+
+	void rotarCara(int cara[][3]) {
+		int temp[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				temp[i][j] = cara[2 - j][i];
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				cara[i][j] = temp[i][j];
+			}
+		}
 	}
 
 	int* intercambiar(int lineaPre[3], int lineaPos[3]) {
@@ -59,7 +130,6 @@ public:
 	}
 
 	//temp
-
 
 	void imprimiRubik() {
 
